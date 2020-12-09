768539b01a718d2333bd6edf4d688f802ffec061
Beven Nyamande <bevennyamande@gmail.com>
2020-12-09 23:19:47 +0200

    learning js

---
+++ lesson.js
@@ -0,0 +1,32 @@
+// comments
+
+// defining variables
+// var , let, const
+var item = 1 // global variables
+let name = 'five' // block scoped variables
+const pie = 3.212444 // constants
+
+// Conditionals
+if ( 5 == 5 ) {
+console.log('five equals to five');
+}
+else if ( 10 == 10 ){
+    console.log('ten equals ten');
+}
+else {
+console.log('nothing to compare')
+};
+
+//  case 23;
+
+//  Functions
+const func = (a, b) => {
+
+    let nums = a * b;
+
+    console.log(nums); // Outputs 250
+
+}
+
+func(25, 10);
+
